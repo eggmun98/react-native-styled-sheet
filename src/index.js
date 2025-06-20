b654a6035b97670df7491dc79d236ac5e8884610
eggmun <eggmun98@gmail.com>
2025-06-20 19:40:47 +0900

    feat: add support for 16 React Native components

--- src/index.js
+++ src/index.js
@@ -1,13 +1,37 @@
 import React from 'react';
-import { View } from 'react-native';
+import { 
+  View, 
+  Text, 
+  TouchableOpacity,
+  TouchableHighlight,
+  TouchableWithoutFeedback,
+  Pressable,
+  ScrollView, 
+  Image,
+  ImageBackground,
+  TextInput,
+  FlatList,
+  SectionList,
+  VirtualizedList,
+  SafeAreaView,
+  KeyboardAvoidingView,
+  RefreshControl,
+  // Modal,
+  // ActivityIndicator,
+  // Switch,
+  // StatusBar,
+  // Button
+} from 'react-native';
 import { cssToRN } from './utils/cssToRN';
 
 function styled(Component) {
-  return function (strings, ...values) {
+  return function (strings) {
     return function StyledComponent(props) {
       const cssString = strings.join('');
+      console.log('받은 CSS 문자열:', cssString);
       
       const style = cssToRN(cssString);
+      console.log('변환된 스타일:', style);
 
       return React.createElement(Component, {
         ...props,
@@ -18,5 +42,22 @@ function styled(Component) {
 }
 
 styled.View = styled(View);
+styled.Text = styled(Text);
+styled.TouchableOpacity = styled(TouchableOpacity);
+styled.TouchableHighlight = styled(TouchableHighlight);
+styled.TouchableWithoutFeedback = styled(TouchableWithoutFeedback);
+styled.Pressable = styled(Pressable);
+styled.ScrollView = styled(ScrollView);
+styled.Image = styled(Image);
+styled.ImageBackground = styled(ImageBackground);
+styled.TextInput = styled(TextInput);
+styled.FlatList = styled(FlatList);
+styled.SectionList = styled(SectionList);
+styled.VirtualizedList = styled(VirtualizedList);
+styled.SafeAreaView = styled(SafeAreaView);
+styled.KeyboardAvoidingView = styled(KeyboardAvoidingView);
+styled.RefreshControl = styled(RefreshControl);
+
+
 
 export default styled; 

eb8c4728f789d7aa0d29a2da718a9d3dadfb0e53
eggmun <eggmun98@gmail.com>
2025-06-20 20:50:48 +0900

    feat: add attrs support and JSDoc comments to createStyledComponent

--- src/utils/createStyledComponent.js
+++ src/utils/createStyledComponent.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import { cssToRN } from './cssToRN';
 
-export const createStyledComponent = (Component, strings) => {
+/**
+ * styled component를 생성하는 핵심 함수
+ * @param {React.ComponentType} Component - 래핑할 React Native 컴포넌트
+ * @param {string[]} strings - 템플릿 리터럴의 문자열 배열
+ * @param {object} attrsProps - attrs()로 전달된 기본 props (선택적)
+ * @returns {React.ComponentType} 스타일이 적용된 React 컴포넌트
+ */
+export const createStyledComponent = (Component, strings, attrsProps) => {
   return function StyledComponent(props) {
     const cssString = strings.join('');
     
     const style = cssToRN(cssString);
 
+    const finalProps = attrsProps ? { ...attrsProps, ...props } : props;
+
     return React.createElement(Component, {
-      ...props,
-      style: [style, props.style],
+      ...finalProps,
+      style: [style, finalProps.style],
     });
   };
 }; 
